3bec473e66894520778a3218337e660e46b915aa
murshad743 <mypc743@gmail.com>
2024-08-27 13:49:43 +0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello {
     public static void main(String[] args) {
         // Loop to print "Hello World" 10 times
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hello Worl walasdasdedd");
+            System.out.println("Hello Worl walasdasdlkssdedd");
         }
     }
 }
