8ec81007ba18570a67b8f538706351451965e29b
alpha <mypc743@gmail.com>
2024-08-27 13:35:22 +0500

    commited the file

---
+++ hello.java
@@ -0,0 +1,9 @@
+package hello;
+public class hello {
+    public static void main(String[] args) {
+        // Loop to print "Hello World" 10 times
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello Worl waleedd");
+        }
+    }
+}
